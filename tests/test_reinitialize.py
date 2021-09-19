4466a5c02b5241ee4e80038c87721ac0b593723a
Shahul ES <shahules@Shahuls-MacBook-Air.local>
2021-09-19 21:31:15 +0530

    test reinitization

--- tests/test_reinitialize.py
+++ tests/test_reinitialize.py
@@ -1,18 +1,22 @@
+import torch
 import unittest
+from torch import nn as nn
 from transformers import AutoModel
 from stabilizer.model import PoolerClassifier
 from stabilizer.reinitialize import reinit_autoencoder_model
+from transformers import logging
+logging.set_verbosity_error()
 
 
 class TestReinitialize(unittest.TestCase):
     def test_reinit_autoencoder_model(self):
         transformer = AutoModel.from_pretrained(
-            pretrained_model_name_or_path="models/bert-base-uncased/",
+            pretrained_model_name_or_path="bert-base-uncased",
             hidden_dropout_prob=0.1,
             attention_probs_dropout_prob=0.1,
         )
         transformer.encoder = reinit_autoencoder_model(
-            transformer.encoder, reinit_num_layers=2
+            transformer.encoder, reinit_num_layers=1
         )
         model = PoolerClassifier(
             transformer=transformer,
@@ -20,9 +24,20 @@ class TestReinitialize(unittest.TestCase):
             transformer_output_dropout_prob=0.1,
             num_classes=1,
         )
-        self.assertEqual(
-            model.transformer.encoder.config.attention_probs_dropout_prob, 0.1
-        )
+        
+        vals=[]
+        for module in model.transformer.encoder.layer[-1].modules():
+    
+            if isinstance(module,nn.Linear):
+                k = torch.isclose(module.weight.data.mean(),torch.tensor(0.),atol=1e-4)
+                v = torch.isclose(module.weight.data.std().detach().cpu(),torch.tensor(0.02),atol=1e-4)
+                vals.append(k.cpu().numpy())
+                vals.append(v.cpu().numpy())
+                vals.append(all(module.bias.data.detach().cpu()==torch.zeros(module.bias.data.shape)))
+                
+        
+        self.assertEqual(all(vals),True)
+    
 
 
 if __name__ == "__main__":
