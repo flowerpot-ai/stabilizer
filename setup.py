987b0e816dcdb22d2789a849015b8d61007de19d
Shahul ES <shahules786@gmail.com>
2021-10-01 10:28:19 +0530

    remove py 3.6 support

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     install_requires=required,
     include_package_data=True,
 )
